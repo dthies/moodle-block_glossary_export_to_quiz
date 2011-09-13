9a13cfb2a4700a6872aeef3bf77b63cbc419c8cb
Joseph Rezeau <joseph@rezeau.org>
2011-09-13 23:08:42 +0200

    added GNU blabla
    Signed-off-by: Joseph Rezeau <joseph@rezeau.org>

--- block_glossary_export_to_quiz.php
+++ block_glossary_export_to_quiz.php
@@ -1,4 +1,29 @@
-<?php // $Id: block_glossary_export_to_quiz.php,v 1.0. 2010/11/27 01:01:01 rezeau Exp $
+<?php 
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ 
+/**
+ * Glossary Export block definition
+ *
+ * @package    contrib
+ * @subpackage block_glossary_export_to_quiz
+ * @copyright  2011 Joseph Rézeau
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later (5)
+ */
 
 class block_glossary_export_to_quiz extends block_base {
     function init() {
